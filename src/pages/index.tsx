92d72d04f3bb646638b257af653e91d75f8be163
Murilo Morandi <murilomorandialexandre2011@gmail.com>
2022-01-03 23:54:37 +0044

    Configuração incial do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | News Leader</title>
+      </Head>
+
+      <h1>
+        Home
+      </h1>
+    </>
+  )
+}
