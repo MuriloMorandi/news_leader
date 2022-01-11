a0c52892992046a86b70ae858d5b1b939aa31bce
Murilo Morandi <murilomorandialexandre2011@gmail.com>
2022-01-11 21:08:59 +0044

    Integração com stripe

--- src/components/SubscribrButton/index.tsx
+++ src/components/SubscribrButton/index.tsx
@@ -1,6 +1,11 @@
+import { Interface } from 'readline';
 import styles from './styles.module.scss';
 
-export function SubscribeButton() {
+interface SubscribeButtonProps{
+    priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     return(
         <button type="button" className={styles.SubscribeButton}>
             Subscribe Now
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,36 @@
-import { SubscribeButton } from 'components/SubscribrButton';
 import Head from 'next/head';
-import styles from 'styles/home.module.scss'
+import { GetStaticProps } from 'next';
 
-export default function Home() {
+import { SubscribeButton } from 'components/SubscribrButton';
+import styles from 'styles/home.module.scss'
+import { stripe } from 'service/stripe';
+
+interface HomeProps{
+  product:{
+    priceId: string;
+    amount: string;
+  }
+}
+
+export const getStaticProps  = async (params:GetStaticProps) => {
+  const price = await stripe.prices.retrieve('price_1KGsi5DmWZ4Pfu9OAH6NLIlb')
+  
+  const props:HomeProps = {
+    product: {
+      priceId: price.id,
+      amount: new Intl.NumberFormat('en-US', {
+        style:'currency',
+        currency: price.currency
+      }).format(price.unit_amount / 100)
+    }
+  }
+
+  return {
+    props:props,
+    revalidate: 60 * 2 //2 min
+  }
+}
+export default function Home({ product }:HomeProps) {
   return (
     <>
       <Head>
@@ -17,9 +45,9 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all the publications <br/>
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         
         <img src='/img/avatar.svg' alt='Girl coding'/>
